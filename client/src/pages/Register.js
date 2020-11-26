3343974ad832488b67d698b8c751455695e82526
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-11-27 00:04:19 +0800

    added style to form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import axios from "axios";
+import '../App.css'
 
 
 function Register(props) {
@@ -31,17 +32,18 @@ function Register(props) {
     }
 
     return (
-        <>
-            Register
-            {JSON.stringify(user)}
-            <form onSubmit={handleSubmit}>
-                <label>Email</label>
-                <input type="text" value={user.email} name="email" onChange={handleInputs}/>
-                <label>Password</label>
-                <input type="password" value={user.password} name="password" onChange={handleInputs}/>
-                <button>Submit</button>
-            </form>
-        </>
+        <div className="form-container">
+            <div className="form-wrapper">
+                <h2>Create Account</h2>
+                <form onSubmit={handleSubmit}>
+                    <input placeholder="Email" type="text" value={user.email} name="email" onChange={handleInputs} autoComplete="off"/>
+                    <p className="error-message">Email already exists</p>
+                    <input placeholder="Password" type="password" value={user.password} name="password" onChange={handleInputs}/>
+                    <p className="error-message">Wrong password</p>
+                    <button>Sign Up</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
